b60890cf38bd9ee89643d7c5325d74380bbb1dd7
nabon <nabongirma2@gmail.com>
2024-01-26 23:39:23 +0300

    postjob php

---
+++ provider/postjobpage/header.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="postjob.css" />
+  <style>
+ body {
+      font-family: Arial, sans-serif;
+      margin: 0;
+      padding: 0;
+      background-color: #f4f4f4;
+    }
+
+    .header {
+      background-color: #3795b3;
+      color: #fff;
+      padding: 10px;
+      text-align: center;
+    }
+
+    .links {
+      margin-top: 10px;
+      text-align: center;
+     
+    }
+
+    .links ul {
+      list-style-type: none;
+      padding: 0;
+      margin: 0;
+    }
+
+    .links li {
+      display: inline-block;
+      margin: 0 10px;
+    }
+
+    .links a {
+      text-decoration: none;
+      color: #3795b3;
+      padding: 5px 10px;
+      border: 2px solid #3795b3;
+      border-top: none;
+      border-left: none;
+      border-right: none;
+      border-radius: 5px;
+      background-color: #fff;
+    }
+
+    .links a:hover {
+      background-color: #3795b3;
+      color: #fff;
+    }
+    .navcontainer{
+      /* position: fixed; */
+      align-content: center;
+      width:100%;
+    }
+ 
+  </style>
+</head>
+<body>
+  <div class="navcontainer">
+  <!-- <div class="header">
+    <h1>SERALINK</h1>
+  </div> -->
+
+  <div class="links">
+    <ul>
+      <li><a href="index.php">Home</a></li>
+      <li><a href="#">Profile</a></li>
+      <li><a href="#">Signup</a></li>
+      <li><a href="#">Feedback</a></li>
+    </ul>
+  </div>
+  </div>
